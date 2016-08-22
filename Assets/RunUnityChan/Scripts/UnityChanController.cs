762d70a0dea160db717ded6e2db59d3198f194a7
Makoto Ishihara <los_tau_ern330@yahoo.co.jp>
2016-08-22 15:17:42 +0900

    first commit

---
+++ Assets/RunUnityChan/Scripts/UnityChanController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityChanController : MonoBehaviour {
+
+	public void OnTapped(){
+        this.GetComponent<Animator>().SetTrigger("jump");
+	}
+
+    public void OnCollidedWithObstacle() {
+        this.GetComponent<Animator>().SetTrigger("Collision");
+    }
+	
+	// Update is called once per frame
+	public void OnCallFace() {
+	
+	}
+}
