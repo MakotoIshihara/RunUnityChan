a297b582bb135c6c029c576b8f84db7540499b4c
Makoto Ishihara <los_tau_ern330@yahoo.co.jp>
2016-08-25 18:35:03 +0900

    しゃがみ動作を追加

--- Assets/RunUnityChan/Scripts/RunUnityChanController.cs
+++ Assets/RunUnityChan/Scripts/RunUnityChanController.cs
@@ -6,28 +6,50 @@ public class RunUnityChanController : MonoBehaviour {
     [SerializeField]
     private UnityChanController unityChanController;
     [SerializeField]
-    private GameObject obstaclePrefab;
+    //private GameObject obstaclePrefab;
     private float elapsedTime = 0.0f;
     private bool isGameOver = false;
+    public GameObject[] obstacleArray;
 
     void Update(){
-        if (Input.GetMouseButtonDown(0)){
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit raycastHit;
-            if(Physics.Raycast(ray, out raycastHit)) {
-                if (raycastHit.transform.gameObject.tag.Contains("UnityChan")) {
-                    this.unityChanController.OnTapped();
-                }
-            }
+       
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            this.unityChanController.Jump();
+        } else {
+            if (Input.GetKey(KeyCode.DownArrow)) {
+                this.unityChanController.Down();
+            } else {
+                this.unityChanController.DownToUp();
+            } 
         }
+
+        //if (Input.GetMouseButtonDown(0)){
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    RaycastHit raycastHit;
+        //    if(Physics.Raycast(ray, out raycastHit)) {
+        //        if (raycastHit.transform.gameObject.tag.Contains("UnityChan")) {
+        //            this.unityChanController.OnTapped();
+        //        }
+        //    }
+        //}
 
         elapsedTime += Time.deltaTime;
         if(3.0f <= elapsedTime) {
-            GameObject obstacle = Instantiate(this.obstaclePrefab);
+            int number = Random.Range(0, obstacleArray.Length);
+            GameObject obstacle = Instantiate(obstacleArray[number]);
             ObstacleController obstacleController = obstacle.GetComponent<ObstacleController>();
             obstacleController.CollidedWithUnityChan += this.ObstacleCollidedWithUnityChan;
-
-            obstacle.transform.position = new Vector3(0.0f, 0.0f, 3.0f);
+            switch (obstacle.tag) {
+                case "broadBar":
+                    obstacle.transform.position = new Vector3(0.0f, 1.2f, 3.0f);
+                    break;
+                case "bar":
+                    obstacle.transform.position = new Vector3(0.0f, 0.0f, 3.0f);
+                    break;
+                default:
+                    break;
+            }
+            //obstacle.transform.position = new Vector3(0.0f, 0.0f, 3.0f);
             elapsedTime = 0.0f;
         }
     }
--- Assets/RunUnityChan/Scripts/UnityChanController.cs
+++ Assets/RunUnityChan/Scripts/UnityChanController.cs
@@ -3,16 +3,24 @@ using System.Collections;
 
 public class UnityChanController : MonoBehaviour {
 
-	public void OnTapped(){
+	public void Jump(){
         this.GetComponent<Animator>().SetTrigger("jump");
 	}
+
+    public void Down() {
+        this.GetComponent<Animator>().SetBool("Down", true);
+    }
+
+    public void DownToUp() {
+        this.GetComponent<Animator>().SetBool("Down", false);
+    }
 
     public void OnCollidedWithObstacle() {
         this.GetComponent<Animator>().SetTrigger("Collision");
     }
 	
 	// Update is called once per frame
-	public void OnCallFace() {
+	public void OnCallChangeFace() {
 	
 	}
 }
